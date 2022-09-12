240af1d66c84a371b77aafdb82272c367bc7f62c
Doğancan Mavideniz <dogancanmavideniz@gmail.com>
2022-09-12 22:34:56 +0300

    🖍 Current Task component developed.

---
+++ Timefy/ViewControllers/CurrentTaskComponent/CurrentTaskComponent.swift
@@ -0,0 +1,23 @@
+//
+//  CurrentTaskComponent.swift
+//  Timefy
+//
+//  Created by Doğancan Mavideniz on 12.09.2022.
+//
+
+import UIKit
+
+class CurrentTaskComponent: UIView {
+
+    @IBOutlet private weak var timeTextLabel: UILabel!
+    @IBOutlet private weak var projectNameLabel: UILabel!
+    @IBOutlet private weak var goDetailButton: UIButton!
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
